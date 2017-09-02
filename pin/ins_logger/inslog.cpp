5b4f84a8bcceda16cc5a50f1cef5a41aedebbc55
David Bidner <david.bidner@student.tugraz.at>
2017-09-02 10:02:57 +0200

    IMP: log only calls and branches

--- pin/ins_logger/inslog.cpp
+++ pin/ins_logger/inslog.cpp
@@ -1,16 +1,27 @@
 #include <stdio.h>
+#include <unordered_map>
 #include "pin.H"
 
 FILE * trace;
+static std::unordered_map<ADDRINT, std::string> str_of_ins_at;
+
+VOID printins(ADDRINT addr)
+{
+  std::string ins_str = str_of_ins_at[addr];
+  fprintf(trace, "[%s]\n", ins_str.c_str());
+}
 
 VOID Instruction(INS ins, VOID *v)
 {
-  fprintf(trace,"[%s]\n",(INS_Disassemble(ins)).c_str());
+  if(INS_IsBranchOrCall(ins))
+  {
+    str_of_ins_at[INS_Address(ins)] = INS_Disassemble(ins);
+    INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)printins, IARG_ADDRINT, INS_Address(ins), IARG_END);
+  }
 }
 
 VOID Fini(INT32 code, VOID *v)
 {
-  fprintf(trace, "#eof\n");
   fclose(trace);
 }
 
