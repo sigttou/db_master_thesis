31a1fb226f14e459e740630fdaa75b688dd9b2c9
David Bidner <david.bidner@student.tugraz.at>
2017-09-02 09:16:22 +0200

    IMP: 2 spaces tabs

--- pin/ins_logger/inslog.cpp
+++ pin/ins_logger/inslog.cpp
@@ -10,28 +10,24 @@ VOID Instruction(INS ins, VOID *v)
 
 VOID Fini(INT32 code, VOID *v)
 {
-    fprintf(trace, "#eof\n");
-    fclose(trace);
+  fprintf(trace, "#eof\n");
+  fclose(trace);
 }
 
 INT32 Usage()
 {
-    PIN_ERROR( "This Pintool logs instructions\n" 
-              + KNOB_BASE::StringKnobSummary() + "\n");
-    return -1;
+  PIN_ERROR( "This Pintool logs instructions\n" 
+            + KNOB_BASE::StringKnobSummary() + "\n");
+  return -1;
 }
-
-/* ===================================================================== */
-/* Main                                                                  */
-/* ===================================================================== */
 
 int main(int argc, char *argv[])
 {
-    if (PIN_Init(argc, argv)) 
-      return Usage();
-    trace = fopen("inslog.out", "w");
-    INS_AddInstrumentFunction(Instruction, 0);
-    PIN_AddFiniFunction(Fini, 0);
-    PIN_StartProgram();
-    return 0;
+  if (PIN_Init(argc, argv)) 
+    return Usage();
+  trace = fopen("inslog.out", "w");
+  INS_AddInstrumentFunction(Instruction, 0);
+  PIN_AddFiniFunction(Fini, 0);
+  PIN_StartProgram();
+  return 0;
 }
