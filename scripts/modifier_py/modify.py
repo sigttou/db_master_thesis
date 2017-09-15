d82e5805cb4d3fde97f17244c08fb4bf55683723
David Bidner <david.bidner@student.tugraz.at>
2017-09-15 12:58:57 +0200

    IMP: better management of modifications

--- scripts/modifier_py/modify.py
+++ scripts/modifier_py/modify.py
@@ -1,34 +1,164 @@
 #!/usr/bin/env python3
 """
-    modifies a given binary on the given address
+    expects following input:
+    ./modify.py <path_to_binary> <succ_outfile> <mod_instr> <fail_params>
+    mod_inst may look like that:
+    {file} {address}
+    the succ_outfile includes a json with
+    {
+        "ret": <retval>,
+        "stdout": <stdout>,
+        "stderr": <stderr>
+    }
 """
 import sys
+import subprocess
+import os
+import shutil
+import contextlib
+import json
+import itertools
+from pprint import PrettyPrinter
 import capstone
 from capstone import CS_ARCH_X86, CS_MODE_64
 from bitstring import BitArray
+from parse import parse
 
 
 DIAS = capstone.Cs(CS_ARCH_X86, CS_MODE_64)
+TMPDIR = "./tmpfiles/"
+PP = PrettyPrinter()
 
 
 def main(args):
     """
         receives arguments and checks for possible modifications
     """
-    file = args[0]
-    addr = int(args[1], 16)
-    print("Checking for modifications in {} at {}".format(file, addr))
-    print(get_mods_from_addr(file, addr))
+    binary = os.path.abspath(args[0])
+    fail_params = args[3]
+    files, mods = get_mods(args[2])
+    with open(args[1]) as f:
+        succ_info = json.load(f)
+    check_mods(binary, succ_info, fail_params, files, mods)
     return 0
+
+
+def check_mods(binary, succ_info, fail_params, files, modifications):
+    """
+        checks which modification generates the same output
+    """
+    libs = [e for e in files if e != binary]
+    for f in files:
+        shutil.copy(f, TMPDIR + os.path.basename(f))
+        shutil.copymode(f, TMPDIR + os.path.basename(f))
+    for i in range(len(modifications)):
+        for mods in itertools.combinations(modifications, i+1):
+            to_modify = []
+            tmp_mods = list(mods)
+            for entry in mods:
+                fname, addr, op_len, ops = entry
+                tmp_mods.remove(entry)
+                for op in ops:
+                    to_mod = [(fname, addr, op_len, op)]
+                    if tmp_mods:
+                        for j in range(max([len(x[3]) for x in tmp_mods])):
+                            to_mod = [(fname, addr, op_len, op)]
+                            for m_f, m_a, m_l, m_ops in tmp_mods:
+                                if len(m_ops) > j:
+                                    to_mod.append((m_f, m_a, m_l, m_ops[j]))
+                                    to_modify.append(to_mod)
+                    else:
+                        to_modify.append(to_mod)
+            print()
+            PP.pprint(list(to_modify))
+            print()
+    exit(0)
+    # apply every possible modification 
+    mods = []
+    for entry in [m for m in modifications]:
+        entry_mods = modifications[entry]
+        for addr, op_len, mods in entry_mods:
+            for mod in mods:
+                ret_dict = {}
+                ret_dict["stdout"] = ""
+                ret_dict["stderr"] = ""
+                ret_dict["ret"] = 0
+                tempfile = TMP_LIB
+                if entry == binary:
+                    tempfile = TMP_EXEC
+                modify_bin(entry, tempfile, addr, op_len, mod)
+                try:
+                    if entry == binary:
+                        ret_dict["stdout"] = subprocess.check_output([tempfile]
+                                                                     + fail_params.split()
+                                                                    ).decode("utf8")
+                    else:
+                        env = os.environ
+                        env["LD_PRELOAD"] = os.path.abspath(tempfile)
+                        ret_dict["stdout"] = subprocess.check_output([binary]
+                                                                     + fail_params.split(), env=env
+                                                                    ).decode("utf8")
+                except subprocess.CalledProcessError as e:
+                    ret_dict["ret"] = e.returncode
+                    ret_dict["stdout"] = e.stdout.decode("utf8")
+                    ret_dict["stderr"] = e.stdout.decode("utf8")
+                if ret_dict == succ_info:
+                    print("Success for {} at {} in {}".format(mod[0], addr, entry))
+
+                with contextlib.suppress(FileNotFoundError):
+                    shutil.rmtree(LIB_FOLDER + "*")
+
+
+def modify_bin(binfile, tmpfile, addr, op_len, mod):
+    """
+        modifies the given binary file and places it in the tmp
+    """
+    addr = int(addr, 16)
+    instr = mod[1]
+    with open(binfile, "rb") as f:
+        content = bytearray(f.read())
+    if op_len == 16:
+        content[addr] = instr[0]
+        content[addr+1] = instr[1]
+    else:
+        content[addr] = int(instr.hex(), 16)
+    with open(tmpfile, "wb") as f:
+        f.write(content)
+    shutil.copymode(binfile, tmpfile)
+
+
+def get_mods(file):
+    """
+        Loads a file in the format
+        <addr> <file>
+        generates all possible modifications
+        returns a list containing:
+        (<filename>, <addr>, (<opcode_name>, <len>, <bytes>))
+    """
+    ret = []
+    files = set()
+    with open(file) as f:
+        entries = f.readlines()
+
+    for e in entries:
+        result = parse("{addr} {path}", e)
+        path = result["path"]
+        files.add(path)
+        addr = result["addr"]
+        tmp_len, tmp_mods = get_mods_from_addr(path, int(addr, 16))
+        tmp_s = (path, addr, tmp_len, tmp_mods)
+        ret.append(tmp_s)
+    return list(files), ret
 
 
 def get_mods_from_addr(file, addr):
     """
         returns a triple including address, instruction length and the possible mods
     """
+    ret = []
     instr = get_instr_from_addr(file, addr)
-    mods = (hex(addr), len(instr), get_possible_mods(instr))
-    return mods
+    ret += get_possible_mods(instr)
+    return len(instr), ret
 
 
 def get_instr_from_addr(file, addr):
@@ -47,6 +177,7 @@ def get_instr_from_addr(file, addr):
         else:
             instr = BitArray(hex=str(hex(instr)))
     return instr
+
 
 def get_possible_mods(opcode):
     """
---
+++ scripts/modifier_py/pin_run.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+"""
+    expects following input:
+    ./pin_run.py <binary> <succ_params> <fail_params>
+    if will generate following files:
+        mod_instr.out
+        succ.out
+        fail.out
+"""
+import sys
+import os
+import subprocess
+import difflib
+import json
+from parse import parse
+
+SUCC_PINOUT = "succpin.out"
+FAIL_PINOUT = "failpin.out"
+SUCC_OUT = "succ.out"
+MOD_FILE = "mod_instr.out"
+PIN_BIN = "/home/user/.local/bin/pin"
+PIN_TOOL = "/home/user/MSC/db_master_thesis/pin/branch_logger/obj-intel64/branchlog.so"
+
+def main(args):
+    """
+        receives arguments and checks for possible modifications
+    """
+    binary = os.path.abspath(args[0])
+    success_params = args[1]
+    fail_params = args[2]
+
+    pin_run(binary, success_params, PIN_TOOL, SUCC_PINOUT, SUCC_OUT)
+    pin_run(binary, fail_params, PIN_TOOL, FAIL_PINOUT, os.devnull)
+    gen_addrdiff_file(SUCC_PINOUT, FAIL_PINOUT, MOD_FILE)
+    os.remove(SUCC_PINOUT)
+    os.remove(FAIL_PINOUT)
+    return 0
+
+
+def pin_run(binary, parameters, pin_tool, pinoutfile, outfile):
+    """
+        calls the pintoll and logs its output to the given file
+    """
+    output = b""
+    stderr = b""
+    retval = 0
+    try:
+        output = subprocess.check_output([PIN_BIN, "-t", pin_tool, "-o", pinoutfile, "--", binary]
+                                         + parameters.split())
+    except subprocess.CalledProcessError as e:
+        retval = e.returncode if e.returncode else 0
+        stderr = e.stderr
+        output = e.stdout
+    if not output:
+        output = ""
+    ret_dict = {}
+    ret_dict["ret"] = retval
+    ret_dict["stdout"] = output.decode("utf8")
+    ret_dict["stderr"] = stderr.decode("utf8")
+    with open(outfile, "w") as json_f:
+        json.dump(ret_dict, json_f)
+
+def gen_addrdiff_file(a_file, b_file, outfile):
+    """
+        generates an info file out of the differences between the files
+    """
+    with open(a_file, "r") as a:
+        with open(b_file, "r") as b:
+            diff = difflib.ndiff(a.readlines(), b.readlines())
+
+    branches = {}
+    for l in diff:
+        if l.startswith("+") or l.startswith("-"):
+            result = parse("{sym} {addr} - [{instr} {target}] in {path} {branchtaken}", l)
+            addr = result["addr"]
+            path = result["path"]
+            if not branches.get(path):
+                branches[path] = {}
+            if branches[path].get(addr):
+                branches[path][addr]["cnt"] += 1
+            else:
+                branches[path][addr] = {"instr": result["instr"],
+                                        "target": result["target"],
+                                        "cnt": 1
+                                       }
+    with open(outfile, "w") as f:
+        for path in branches:
+            branch = branches[path]
+            for addr in branch:
+                if branch[addr]["cnt"] > 1:
+                    print("{} {}".format(addr, path), file=f)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("<binary> <succ_params> <fail_params>")
+        exit()
+    main(sys.argv[1:])
