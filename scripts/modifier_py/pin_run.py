f7be731a3d5cf812f96e1f0ed8d797dab1f8b4ec
David Bidner <david.bidner@student.tugraz.at>
2017-09-21 10:19:26 +0200

    ADD: stdin file to pin run

--- scripts/modifier_py/pin_run.py
+++ scripts/modifier_py/pin_run.py
@@ -27,10 +27,20 @@ def main(args):
     binary = os.path.abspath(args[0])
     success_params = args[1]
     fail_params = args[2]
+    succ_in = ""
+    fail_in = ""
+    if len(args) > 3 and args[3] == "-i":
+        with open(args[4]) as f:
+            succ_in = f.read().decode("utf8")
+        with open(args[5]) as f:
+            fail_in = f.read().decode("utf8")
+    print(succ_in)
+    print(fail_in)
     CONF_OUT["params"] = fail_params
     CONF_OUT["binary"] = binary
-    pin_run(binary, success_params, PIN_TOOL, SUCC_PINOUT, SUCC_OUT)
-    pin_run(binary, fail_params, PIN_TOOL, FAIL_PINOUT, os.devnull)
+    CONF_OUT["fail_in"] = fail_in
+    pin_run(binary, success_params, PIN_TOOL, SUCC_PINOUT, SUCC_OUT, succ_in)
+    pin_run(binary, fail_params, PIN_TOOL, FAIL_PINOUT, os.devnull, fail_in)
     CONF_OUT["mods"] = gen_addrdiff(SUCC_PINOUT, FAIL_PINOUT)
     # os.remove(SUCC_PINOUT)
     # os.remove(FAIL_PINOUT)
@@ -39,7 +49,7 @@ def main(args):
     return 0
 
 
-def pin_run(binary, parameters, pin_tool, pinoutfile, outfile):
+def pin_run(binary, parameters, pin_tool, pinoutfile, outfile, stdin=None):
     """
         calls the pintoll and logs its output to the given file
     """
@@ -48,7 +58,7 @@ def pin_run(binary, parameters, pin_tool, pinoutfile, outfile):
     retval = 0
     try:
         output = subprocess.check_output([PIN_BIN, "-t", pin_tool, "-o", pinoutfile, "--", binary]
-                                         + parameters.split())
+                                         + parameters.split(), input=stdin)
     except subprocess.CalledProcessError as e:
         retval = e.returncode if e.returncode else 0
         stderr = e.stderr
@@ -98,7 +108,8 @@ def gen_addrdiff(a_file, b_file):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
+    if len(sys.argv) != 4 and sys.argv[4] != "-i":
         print("<binary> <succ_params> <fail_params>")
+        print("<binary> <succ_params> <fail_params> -i <succ_stdinfile> <fail_stdinfile>")
         exit()
     main(sys.argv[1:])
