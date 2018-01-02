ba9bf5874381d7009cfcc2d9503e89ad74c9b6ca
David Bidner <david.bidner@student.tugraz.at>
2018-01-02 10:59:25 +0100

    ADD: proper logging

--- pin/access_logger/acclog.cpp
+++ pin/access_logger/acclog.cpp
@@ -1,37 +1,21 @@
 #include <stdio.h>
 #include <unordered_map>
 #include <set>
+#include <iostream>
 #include "pin.H"
 
+FILE * trace;
+
 static std::unordered_map<ADDRINT, std::string> str_of_img_at;
-static std::set<std::pair<ADDRINT, ADDRINT>> memory_accesses;
+static std::set<std::pair<VOID*, ADDRINT>> memory_accesses;
 static std::set<std::pair<ADDRINT, ADDRINT>> instructions;
 
 KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool",
   "o", "branchlog.out", "specify output file name");
 
-VOID printbranch(ADDRINT addr, ADDRINT target)
+VOID RecordMemRead(VOID* ip, VOID* addr, ADDRINT base)
 {
-  std::string ins_str = str_of_ins_at[addr];
-  // fprintf(trace, "[%s] - branching at %zx - target: %zx\n", ins_str.c_str(), addr, target);
-  cur_branch = addr;
-  branch_target = target;
-  was_branch = true;
-}
-
-VOID printins(ADDRINT addr, ADDRINT base)
-{
-  if(was_branch)
-  {
-    std::string ins_str = str_of_ins_at[cur_branch];
-    std::string img_str = str_of_img_at[base];
-    fprintf(trace, "%zx - [%s] in %s ", cur_branch-base, ins_str.c_str(), img_str.c_str());
-    if(branch_target == addr)
-      fprintf(trace, "T\n");
-    else
-      fprintf(trace, "N\n");
-  }
-  was_branch = false;
+  memory_accesses.insert(std::make_pair(addr, base));
 }
 
 VOID ImageLoad(IMG img, VOID *v)
@@ -46,36 +30,33 @@ VOID Instruction(INS ins, VOID *v)
   ADDRINT base = 0x0;
   if(IMG_Valid(img))
       base = IMG_LowAddress(img);
-  instructions.insert(std::make_pair(base, ins_addr));
+  instructions.insert(std::make_pair(ins_addr, base));
 
   UINT32 memOperands = INS_MemoryOperandCount(ins);
   for (UINT32 memOp = 0; memOp < memOperands; memOp++)
   {
-    if (INS_MemoryOperandIsRead(ins, memOp))
-    {
-      INS_InsertPredicatedCall(
-        ins, IPOINT_BEFORE, (AFUNPTR)RecordMemRead,
-        IARG_INST_PTR,
-        IARG_MEMORYOP_EA, memOp,
-        IARG_END);
-    }
-    // Note that in some architectures a single memory operand can be
-    // both read and written (for instance incl (%eax) on IA-32)
-    // In that case we instrument it once for read and once for write.
-    if (INS_MemoryOperandIsWritten(ins, memOp))
-    {
-      INS_InsertPredicatedCall(
-        ins, IPOINT_BEFORE, (AFUNPTR)RecordMemWrite,
-        IARG_INST_PTR,
-        IARG_MEMORYOP_EA, memOp,
-        IARG_END);
-    }
+    INS_InsertPredicatedCall(
+      ins, IPOINT_BEFORE, (AFUNPTR)RecordMemRead,
+      IARG_INST_PTR,
+      IARG_MEMORYOP_EA, memOp,
+      IARG_ADDRINT, base,
+      IARG_END);
   }
 
 }
 
 VOID Fini(INT32 code, VOID *v)
 {
+  for(auto it : memory_accesses)
+  {
+    std::string img = str_of_img_at[it.second];
+    fprintf(trace, "%p - %s\n", it.first, img.data());
+  }
+  for(auto it : instructions)
+  {
+    std::string img = str_of_img_at[it.second];
+    fprintf(trace, "0x%zx - %s\n", it.first, img.data());
+  }
   fclose(trace);
 }
 
