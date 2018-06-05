9a6df41f65cdfb0c6b930ea00b33c49fbaf037ad
David Bidner <david.bidner@student.tugraz.at>
2018-06-05 11:43:59 +0200

    IMP: first sudo scripts for automatation

--- scripts/chroot_env/run_sudo_chroot.sh
+++ scripts/chroot_env/run_sudo_chroot.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+
+# $1 path to modified files
+# $2 file to be replaced
+
+cnt=0
+for i in `ls $1` ; do
+  cp $1$i $2
+  su -c "echo wrong | timeout -s 9 0.1 sudo -S whoami 2>&1 | grep root" - user
+done
--- scripts/chroot_env/run_tests.sh
+++ scripts/chroot_env/run_tests.sh
@@ -1,1 +1,17 @@
 #!/usr/bin/env bash
+
+# $1 = path to chroots
+# $2 = folder to modified files
+# $3 = file to be replaced
+
+cmd_file=run_sudo_chroot.sh
+modified_inside="/media/flips"
+
+for i in $1* ; do
+  echo $i
+  cp $cmd_file $i/
+  mkdir -p $i$modified_inside
+  mount --bind $2 $i$modified_inside
+  echo -e "./$cmd_file $modified_inside/ $3" | chroot $i
+done
+
