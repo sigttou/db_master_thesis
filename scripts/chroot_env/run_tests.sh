f6fc31e547b155bbc28744aa201c75b249120943
David Bidner <david.bidner@student.tugraz.at>
2018-06-11 14:56:29 +0200

    IMP: create chroots inside script

--- scripts/chroot_env/run_tests.sh
+++ scripts/chroot_env/run_tests.sh
@@ -1,38 +1,48 @@
 #!/usr/bin/env bash
 
-# $1 = path to chroots
-# $2 = folder to modified files
-# $3 = file to be replaced
+# $1 = path to skeleton chroot
+# $2 = folder to place chroots
+# $3 = folder to modified files
+# $4 = file to be replaced
+# $5 = number of parallel runs
 
-cmd_file=run_sudo_chroot.sh
 modified_inside="/media/flips"
 log_file="/tmp/succ.file"
+chroot_skeleton="/var/chroot/zesty_templ/"
+chroots_path="/media/ramdisk/chroot/"
+cmd_file=$1
+path_to_flips=$2
+file_to_replace=$3
+num_runs=$4
+
+if [ "$#" -ne 4 ]; then
+  echo "Illegal number of parameters"
+  echo "./run_tests.sh <file_to_exec> <modified_files> <file_to_replace> <number_of_parallel_runs>"
+  exit 1
+fi
 
 worker () {
   local run_chroot=$1
-  local run_path=$2
-  local run_file=$3
   cp $cmd_file $run_chroot/ || { echo 'cp failed' ; exit 1; }
   if [ ! -e "$run_chroot$modified_inside" ]; then
     mkdir -p $run_chroot$modified_inside || { echo 'mkdir failed' ; exit 1; }
-    mount --bind $run_path $run_chroot$modified_inside || { echo 'mount failed' ; exit 1; }
+    mount --bind $path_to_flips $run_chroot$modified_inside || { echo 'mount failed' ; exit 1; }
   fi
-  # echo -e "./$cmd_file $modified_inside/ $run_file $log_file" | chroot $run_chroot
+  echo -e "./$cmd_file $modified_inside/ $file_to_replace $log_file" | chroot $run_chroot
   umount $run_chroot$modified_inside
   rmdir $run_chroot$modified_inside
 }
 
-if [ "$#" -ne 3 ]; then
-  echo "Illegal number of parameters"
-  echo "./run_tests.sh <chroots_path> <modified_files> <file_to_replace>"
-  exit 1
-fi
-
-for i in $1* ; do worker "$i" "$2" "$3" & done
+for i in $(seq $num_runs);
+do
+  tmp_chroot=$chroots_path$i
+  echo cp -R $chroot_skeleton $tmp_chroot
+  worker $tmp_chroot
+done
 
 wait
 
-for i in $1* ; do 
+for i in $chroots_path* ; do 
   if [ -f $i$log_file ]; then
     cat $i$log_file 
   else
