8ae9cddd6c502cc4a96a0b3ba6bc12841b499efb
David Bidner <david.bidner@student.tugraz.at>
2018-06-11 10:17:33 +0200

    IMP: better script for triggering test runs

--- scripts/chroot_env/run_tests.sh
+++ scripts/chroot_env/run_tests.sh
@@ -7,11 +7,22 @@
 cmd_file=run_sudo_chroot.sh
 modified_inside="/media/flips"
 
-for i in $1* ; do
-  echo $i
-  cp $cmd_file $i/
-  mkdir -p $i$modified_inside
-  mount --bind $2 $i$modified_inside
-  echo -e "./$cmd_file $modified_inside/ $3" | chroot $i
-done
+worker () {
+  local run_chroot=$1
+  local run_path=$2
+  local run_file=$3
+  cp $cmd_file $run_chroot/ || { echo 'cp failed' ; exit 1; }
+  mkdir -p $run_chroot$modified_inside || { echo 'mkdir failed' ; exit 1; }
+  mount --bind $run_path $run_chroot$modified_inside || { echo 'mount failed' ; exit 1; }
+  echo -e "./$cmd_file $modified_inside/ $run_file" | chroot $run_chroot
+}
 
+if [ "$#" -ne 3 ]; then
+  echo "Illegal number of parameters"
+  echo "./run_tests.sh <chroots_path> <modified_files> <file_to_replace>"
+  exit 1
+fi
+
+for i in $1* ; do worker "$i" "$2" "$3" & done
+
+echo DONE!
