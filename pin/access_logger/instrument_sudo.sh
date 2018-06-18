5205f4fef7dc0a29c46ef18e4aa89fc5f73b133a
David Bidner <david.bidner@student.tugraz.at>
2018-06-18 08:25:34 +0200

    IMP: access_logger added directory independency

--- pin/access_logger/instrument_sudo.sh
+++ pin/access_logger/instrument_sudo.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+pin_path=$(dirname "$0")
+old_path=$PWD
 cp /usr/bin/sudo /tmp/msc_test
 chown root:root /tmp/msc_test
 chmod +x /tmp/msc_test
@@ -7,6 +9,7 @@ chmod u+s /tmp/msc_test
 
 su - user -c "echo wrong | /tmp/msc_test -S whoami" &> /dev/null &
 sleep 1
-./run_pid.sh msc_test `pgrep msc_test`
-
+cd $pin_path
+./run_pid.sh $old_path/msc_test.out `pgrep msc_test`
+cd $old_path
 while kill -0 `pgrep msc_test` 2> /dev/null; do sleep 1; done;
--- pin/access_logger/run_pid.sh
+++ pin/access_logger/run_pid.sh
@@ -3,7 +3,7 @@
 ROOT=/opt/pin/pin
 tmp=`ls | grep cpp`
 NAME=${tmp%.cpp}
-out=$PWD/$(basename $1).out
+out=$1
 echo Storing accesses from PID $2 to $out
 make PIN_ROOT=$ROOT obj-intel64/$NAME.so 
 $ROOT/pin -xyzzy -mesgon log_win -pid $2 -t obj-intel64/$NAME.so -o $out
