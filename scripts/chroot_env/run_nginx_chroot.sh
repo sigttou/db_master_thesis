9c8488414f8a3da3b53b28ba8a332ff9e6e1d296
David Bidner <david.bidner@student.tugraz.at>
2018-06-25 13:48:44 +0200

    IMP: better nginx testcase

--- scripts/chroot_env/run_nginx_chroot.sh
+++ scripts/chroot_env/run_nginx_chroot.sh
@@ -3,19 +3,40 @@
 # $1 path to modified files
 # $2 file to be replaced
 # $3 file to log to
+
+if [ "$#" -ne 3 ]; then
+  echo "Illegal number of parameters"
+  echo "./run <mod_files> <to_replace> <logfile>"
+  exit
+fi
+
 portnum=8`cat /etc/chrootnum`
 cnt=0
 for i in `ls $1` ; do
-  cp $1$i $2
+  while ! cp $1$i $2; do kill -9 $nginxpid &> /dev/null ; done
   sed -i s/80/$portnum/g /etc/nginx/simple_nginx.conf
-  nginx -c /etc/nginx/simple_nginx.conf &
+  nginx -c /etc/nginx/simple_nginx.conf &> /dev/null &
+  nginxpid=$!
 
   # waiting for server to be started
-  while ! curl --silent localhost:$portnum | grep running &> /dev/null ; do sleep 1 ; done
+  retry=0
+  maxRetries=5
+  until [ ${retry} -ge ${maxRetries} ]
+  do
+    echo GET / | netcat localhost $portnum | grep running &> /dev/null
+    retry=$[${retry}+1]
+    sleep 1
+  done
 
-  if curl --silent -u user:wrong localhost:$portnum/protected/ | grep WIN &> /dev/null; then 
+  if [ ${retry} -ge ${maxRetries} ]; then
+    continue
+  fi
+
+  if echo -e "GET /protected/ HTTP/1.1\nHost: localhost \nAuthorization: Basic $(echo -n 'user:wrong' | base64 )\n" | netcat -q 0 localhost 80 | grep WIN &> /dev/null; then
     echo "SUCCESS: $i" >> $3
   fi
-  nginx -s stop
+  nginx -s stop &> /dev/null
+
   while netstat -ln | grep ":$portnum " &> /dev/null; do sleep 1 ; done
+  kill -9 $nginxpid &> /dev/null
 done
