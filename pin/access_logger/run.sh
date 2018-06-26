336503fef33938090dbf70bcc0f65c31658509b3
David Bidner <david.bidner@student.tugraz.at>
2018-06-26 12:29:44 +0200

    FIX: use correct pin in all scripts

--- pin/access_logger/run.sh
+++ pin/access_logger/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-ROOT=/home/user/build/pin
+ROOT=/opt/pin/pin
 tmp=`ls | grep cpp`
 NAME=${tmp%.cpp}
 call=$@
 out=$(basename $1).out
-make PIN_ROOT=$ROOT obj-intel64/$NAME.so && /home/user/.local/bin/pin -t obj-intel64/$NAME.so -o $out -- $call
+make PIN_ROOT=$ROOT obj-intel64/$NAME.so && $ROOT/pin -t obj-intel64/$NAME.so -o $out -- $call
