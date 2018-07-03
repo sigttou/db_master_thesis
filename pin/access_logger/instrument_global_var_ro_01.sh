edd6ee85d9b0b600735767d46f576a6554b9f0e2
David Bidner <david.bidner@student.tugraz.at>
2018-07-03 10:53:22 +0200

    ADD: ro global variable test

---
+++ pin/access_logger/instrument_global_var_ro_01.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+pin_path=$(dirname "$0")
+old_path=$PWD
+
+cd $pin_path
+echo 1 | ./run.sh $old_path/msc_test.out /usr/bin/global_var_ro_01
+cd $old_path
+
+grep global_var_ro_01 $old_path/msc_test.out > /tmp/tmplog
+mv /tmp/tmplog $old_path/msc_test.out
