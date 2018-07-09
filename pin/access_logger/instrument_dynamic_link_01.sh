f0682d7e34c27db11dafa15e25dce319e930947a
David Bidner <david.bidner@student.tugraz.at>
2018-07-09 10:40:12 +0200

    ADD: ELF structure flips to sudo and dynamic instrumentation

--- pin/access_logger/instrument_dynamic_link_01.sh
+++ pin/access_logger/instrument_dynamic_link_01.sh
@@ -5,7 +5,9 @@ old_path=$PWD
 
 cd $pin_path
 echo 1 | ./run.sh $old_path/msc_test.out /usr/bin/dynamic_link_01
-cd $old_path
 
 grep "dynamic_link_01\|libsucc" $old_path/msc_test.out > /tmp/tmplog
 mv /tmp/tmplog $old_path/msc_test.out
+
+../../scripts/elf_structure_flips/add_structures.py $old_path/msc_test.out
+cd $old_path
