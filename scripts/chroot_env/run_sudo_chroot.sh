cdff36a4db8a5741f888921e3445e6f7b6cd1d63
David Bidner <david.bidner@student.tugraz.at>
2018-07-10 13:05:25 +0200

    IMP: use busybox tools where possible

--- scripts/chroot_env/run_sudo_chroot.sh
+++ scripts/chroot_env/run_sudo_chroot.sh
@@ -9,8 +9,8 @@ cnt=0
 for i in `ls $1` ; do
   cp $1$i $2
   chmod u+s /usr/bin/sudo
-  if su -c "echo wrong | timeout -s 9 0.1 sudo -S whoami 2>&1 | grep root" - user > /dev/null 2>&1; then
-    if su -c "echo wrong | sudo -S grep win /checkfile" - user > /dev/null 2>&1 ; then
+  if su -c "busybox echo wrong | busybox timeout -s 9 -t 1 sudo -S busybox whoami 2>&1" - user | grep root > /dev/null 2>&1; then
+    if su -c "busybox echo wrong | sudo -S busybox grep win /checkfile" - user > /dev/null 2>&1 ; then
       echo "SUCCESS: $i - $2" >> $3
       cp $1$i $4$i
     fi
