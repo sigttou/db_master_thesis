d99a8892473a4e0a83aa38f031d9348fa44b729f
David Bidner <david.bidner@student.tugraz.at>
2018-08-11 10:37:46 +0200

    ADD: thesis code

---
+++ binaries/thesis_simple_branch/code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// gcc -O0 -o binary code.c
+
+int main(void)
+{
+    int x = 0;
+    if(x == 1)
+        printf("success.\n");
+    else
+        printf("fail.\n");
+    return 0;
+}
