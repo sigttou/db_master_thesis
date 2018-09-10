a450961767148e3aebb5df52b859a2966a9f4d3d
David Bidner <david.bidner@student.tugraz.at>
2018-09-10 13:14:14 +0200

    ADD: watchdog options for nginx and sshd

--- scripts/runtime_watchdog/watchdog.py
+++ scripts/runtime_watchdog/watchdog.py
@@ -7,9 +7,12 @@ while(1):
     for p in psutil.process_iter():
         try:
             cmd = " ".join(p.cmdline())
-            if("sudo -S" in cmd or "id -u" in cmd or "grep root" in cmd or "grep win" in cmd):
+            # if("sudo -S" in cmd or "id -u" in cmd or "grep root" in cmd or "grep win" in cmd):
+            # if("nginx -c" in cmd or "nginx -s stop" in cmd):
+            if("ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p" in cmd or "sshpass -p" in cmd or
+               "sshd -f /etc/ssh/sshd_test_config" in cmd or "sleep 1" in cmd):
                 age = datetime.datetime.now() - datetime.datetime.fromtimestamp(p.create_time())
-                if(age.seconds > 3):
+                if(age.seconds > 6):
                     try:
                         print(cmd + " " + str(p.pid) + " " + str(age.seconds))
                     except UnicodeEncodeError:
