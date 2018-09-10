5762b3d5979133c86f6cc9d9da62d4a9c1cc22b9
David Bidner <david.bidner@student.tugraz.at>
2018-09-10 08:20:48 +0200

    ADD: script to instrument sshd

---
+++ pin/access_logger/instrument_sshd.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+pin_path=$(dirname "$0")
+old_path=$PWD
+
+/usr/sbin/sshd -f /home/user/MSC/db_master_thesis/configs/sshd/sshd_config -p 2222
+
+sleep 2
+
+cd $pin_path
+./run_pid.sh $old_path/msc_test.out `pgrep -f "sshd_config -p 2222"` &
+
+sleep 2
+
+sshpass -p wrong ssh -p 2222 localhost 2> /dev/null
+
+kill `pgrep -f "sshd_config -p 2222"`
+
+sleep 1
+
+while kill -0 `pgrep -f "sshd_config -p 2222"` 2> /dev/null; do sleep 1; done;
+
+sort -t "-" -k2 -o $old_path/msc_test.out $old_path/msc_test.out
+cd $old_path
