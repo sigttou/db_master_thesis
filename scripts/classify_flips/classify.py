c8e0d8f16386e386aa101610f8205ec67741a12e
David Bidner <david.bidner@student.tugraz.at>
2018-09-24 09:40:33 +0200

    ADD: possibility to classify logfiles completely

--- scripts/classify_flips/classify.py
+++ scripts/classify_flips/classify.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 from capstone import Cs, CS_ARCH_X86, CS_MODE_64
+from parse import parse
 from elftools.elf.elffile import ELFFile
 from elftools.common.exceptions import ELFError
 
@@ -29,22 +30,61 @@ def get_section_name(elf, offset):
     sys.exit(-1)
 
 
-def main(filename, offset):
+def get_disass(data, base_addr):
+    disass = {}
+    md = Cs(CS_ARCH_X86, CS_MODE_64)
+    for (address, size, mnemonic, op_str) in md.disasm_lite(bytes(data), base_addr):
+        disass[address] = (mnemonic, op_str)
+    return disass
+
+
+def print_diff(dis_a, dis_b, offset):
+    search = offset
+    while(not dis_a.get(search)):
+        search -= 1
+    if(dis_a[search][0] != dis_b[search][0]):
+        print("OPCODE")
+    elif(dis_a[search][1] != dis_b[search][1]):
+        print("PARAM")
+    else:
+        print("OTHER")
+        print("ORIG: 0x%x:\t%s\t%s" % (search, dis_a[search][0], dis_a[search][1]))
+        print("DIFF: 0x%x:\t%s\t%s" % (search, dis_b[search][0], dis_b[search][1]))
+
+
+def classify(filename, offset, bit):
     f, elf = get_elf(filename)
     section_name = get_section_name(elf, offset)
-    print(section_name)
     section = elf.get_section_by_name(section_name)
     base_addr = section['sh_addr']
-    md = Cs(CS_ARCH_X86, CS_MODE_64)
-    for (address, size, mnemonic, op_str) in md.disasm_lite(section.data(), base_addr):
-            print("0x%x:\t%s\t%s" % (address, mnemonic, op_str))
+    data = bytearray(section.data())
+
+    dis_a = get_disass(data, base_addr)
+    data[offset - base_addr] ^= 1 << bit
+    dis_b = get_disass(data, base_addr)
+
+    print_diff(dis_a, dis_b, offset)
+
     f.close()
     return
 
 
-if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print("Illegal number of parameters")
-        print("./classify.py <elffile> <offset>")
+def main(logfile):
+    try:
+        with open(logfile, 'r') as f:
+            lines = f.readlines()
+    except FileNotFoundError:
+        print("Err: " + logfile + " not found!")
         sys.exit(-1)
-    main(sys.argv[1], int(sys.argv[2], 16))
+    for l in lines:
+        entry = parse("SUCCESS: {file}_0x{offset}_{bit} - {}", l)
+        if(entry):
+            classify(entry["file"], int(entry["offset"], 16), int(entry["bit"]))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Illegal number of parameters")
+        print("./classify.py <logfile>")
+        sys.exit(-1)
+    main(sys.argv[1])
